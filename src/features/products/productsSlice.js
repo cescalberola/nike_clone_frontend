c3606eafa555653307a1cb389dd12e210abd39c4
Francesc Alberola <f.alberola@gmail.com>
2023-12-11 16:16:04 +0100

    component cart + productSlice

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -3,11 +3,19 @@ import productsService from "./productsService";
 
 const initialState = {
   products: [],
+  cart:[]
 };
 
 export const getAll = createAsyncThunk("products/getAll", async () => {
   try {
     return await productsService.getAll();
+  } catch (error) {
+    console.error(error);
+  }
+});
+export const addCart = createAsyncThunk("products/addCart", async (product) => {
+  try {
+    return product
   } catch (error) {
     console.error(error);
   }
@@ -20,7 +28,10 @@ export const productsSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(getAll.fulfilled, (state, action) => {
       state.products = action.payload;
-    });
+    })
+    .addCase(addCart.fulfilled,(state,action)=>{
+      state.cart= [action.payload,...state.cart]
+    })
   },
 });
 
