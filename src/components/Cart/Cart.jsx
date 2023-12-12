566cca80b9fa72ae12e9b601c43557b55ef8b9b4
Francesc Alberola <f.alberola@gmail.com>
2023-12-12 13:29:29 +0100

    carrito funcionando

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,15 +5,33 @@ import { useSelector } from 'react-redux';
 const Cart = () => {
 const {cart} =useSelector(state=>state.products)
 console.log(cart)
-  return (
-    <>
-   <h1>Cart</h1>
-    </>
-  );
+return (
+  <>
+
+    {cart.length > 0 ? (
+      <ul>
+        {cart.map((product, index) => (
+          <li key={index}>
+            {product.image?.map(img => (
+              <img
+                key={product._id}
+                src={"http://localhost:8080/uploads/" + img}
+                style={{ maxWidth: '100px', maxHeight: '100px' }}
+              />
+            ))}
+            {product.title} 
+            {product.price}
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p>No hay productos en el carrito</p>
+    )}
+  </>
+);
 };
 
 
 
 
-
-export default (Cart);
+export default Cart;
