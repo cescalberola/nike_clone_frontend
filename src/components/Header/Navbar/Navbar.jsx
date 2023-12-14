da2db9c5baca5addaed948b2971a65811ffc6350
Demis Previotto <demis.previotto@gmail.com>
2023-12-14 17:35:37 +0100

    navbar for mobile, user icon for login/logout

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Navbar.scss";
 import Nike from "../../../assets/logos/Nike";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+
+  const { user } = useSelector(state => state.auth)
   return (
     <header className="pre-l-header top-stack-context" data-pre="Header">
       <div className="pre-l-wrapper mauto-sm d-sm-flx">
@@ -4557,47 +4560,91 @@ const Navbar = () => {
               </div>
             </a>
           </div>
-          <a
-            aria-label="Sign In"
-            data-var="mobileProfileIcon"
-            data-type="click_navMembership"
-            data-path="sign in"
-            href="#"
-            className="pre-btn-icon mr2-sm"
-            data-pre="ILink"
-            role="button"
-          >
-            <div className="pre-avatar" data-pre="Avatar">
-              <div className="d-sm-flx flx-ai-sm-c">
-                <div className="icon-btn ripple">
-                  <svg
-                    aria-hidden="true"
-                    className="pre-nav-design-icon"
-                    focusable="false"
-                    viewBox="0 0 24 24"
-                    role="img"
-                    width="24px"
-                    height="24px"
-                    fill="none"
-                    data-var="glyph"
-                    style={{ display: "inline-block" }}
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M12 3a4.5 4.5 0 00-4.5 4.5H9a3 3 0 013-3V3zM7.5 7.5A4.5 4.5 0 0012 12v-1.5a3 3 0 01-3-3H7.5zM12 12a4.5 4.5 0 004.5-4.5H15a3 3 0 01-3 3V12zm4.5-4.5A4.5 4.5 0 0012 3v1.5a3 3 0 013 3h1.5zM4.5 21v-3H3v3h1.5zm0-3a3 3 0 013-3v-1.5A4.5 4.5 0 003 18h1.5zm3-3h9v-1.5h-9V15zm9 0a3 3 0 013 3H21a4.5 4.5 0 00-4.5-4.5V15zm3 3v3H21v-3h-1.5z"
-                    ></path>
-                  </svg>
-                  <img
-                    alt="Avatar"
-                    src=""
-                    className="pre-avatar-image va-sm-m"
-                    style={{ display: "none" }}
-                    data-var="img"
-                  />
+          {user ? (
+            <a
+              aria-label="Sign In"
+              data-var="mobileProfileIcon"
+              data-type="click_navMembership"
+              data-path="sign in"
+              // href='#'
+              className="pre-btn-icon mr2-sm"
+              data-pre="ILink"
+              role="button" onClick={() => dispatchEvent(logout())}
+            >
+              <div className="pre-avatar" data-pre="Avatar">
+                <div className="d-sm-flx flx-ai-sm-c">
+                  <div className="icon-btn ripple">
+                    <svg
+                      aria-hidden="true"
+                      className="pre-nav-design-icon"
+                      focusable="false"
+                      viewBox="0 0 24 24"
+                      role="img"
+                      width="24px"
+                      height="24px"
+                      fill="none"
+                      data-var="glyph"
+                      style={{ display: "inline-block" }}
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M12 3a4.5 4.5 0 00-4.5 4.5H9a3 3 0 013-3V3zM7.5 7.5A4.5 4.5 0 0012 12v-1.5a3 3 0 01-3-3H7.5zM12 12a4.5 4.5 0 004.5-4.5H15a3 3 0 01-3 3V12zm4.5-4.5A4.5 4.5 0 0012 3v1.5a3 3 0 013 3h1.5zM4.5 21v-3H3v3h1.5zm0-3a3 3 0 013-3v-1.5A4.5 4.5 0 003 18h1.5zm3-3h9v-1.5h-9V15zm9 0a3 3 0 013 3H21a4.5 4.5 0 00-4.5-4.5V15zm3 3v3H21v-3h-1.5z"
+                      ></path>
+                    </svg>
+                    <img
+                      alt="Avatar"
+                      src=""
+                      className="pre-avatar-image va-sm-m"
+                      style={{ display: "none" }}
+                      data-var="img"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          </a>
+            </a>
+          ) : (
+            <a
+              aria-label="Sign In"
+              data-var="mobileProfileIcon"
+              data-type="click_navMembership"
+              data-path="sign in"
+              href='/lookup'
+              className="pre-btn-icon mr2-sm"
+              data-pre="ILink"
+              role="button"
+            >
+              <div className="pre-avatar" data-pre="Avatar">
+                <div className="d-sm-flx flx-ai-sm-c">
+                  <div className="icon-btn ripple">
+                    <svg
+                      aria-hidden="true"
+                      className="pre-nav-design-icon"
+                      focusable="false"
+                      viewBox="0 0 24 24"
+                      role="img"
+                      width="24px"
+                      height="24px"
+                      fill="none"
+                      data-var="glyph"
+                      style={{ display: "inline-block" }}
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M12 3a4.5 4.5 0 00-4.5 4.5H9a3 3 0 013-3V3zM7.5 7.5A4.5 4.5 0 0012 12v-1.5a3 3 0 01-3-3H7.5zM12 12a4.5 4.5 0 004.5-4.5H15a3 3 0 01-3 3V12zm4.5-4.5A4.5 4.5 0 0012 3v1.5a3 3 0 013 3h1.5zM4.5 21v-3H3v3h1.5zm0-3a3 3 0 013-3v-1.5A4.5 4.5 0 003 18h1.5zm3-3h9v-1.5h-9V15zm9 0a3 3 0 013 3H21a4.5 4.5 0 00-4.5-4.5V15zm3 3v3H21v-3h-1.5z"
+                      ></path>
+                    </svg>
+                    <img
+                      alt="Avatar"
+                      src=""
+                      className="pre-avatar-image va-sm-m"
+                      style={{ display: "none" }}
+                      data-var="img"
+                    />
+                  </div>
+                </div>
+              </div>
+            </a>
+          )}
           <button
             data-var="mobileButton"
             id="MobileMenuButton"
